a38553e0271fa2963aa6857a849070d1df4b5175
James Ratcliffe <berkeleybross@gmail.com>
2019-04-03 02:05:51 +0100

    Added docker

--- .step1.ps1
+++ .step1.ps1
@@ -4,3 +4,17 @@ sudo snap install dotnet-sdk --classic
 snap install rider --classic
 snap install spotify
 snap install node --channel=10/stable --classic
+
+apt-get update
+apt-get install -y `
+    apt-transport-https `
+    ca-certificates `
+    curl `
+    gnupg-agent `
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository `
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu `
+   $(lsb_release -cs) `
+   stable"
+apt-get install -y docker-ce docker-ce-cli containerd.io
