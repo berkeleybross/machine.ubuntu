ee0f74f5e4deeebdedd3097a0d205d27081eaf7a
James Ratcliffe <berkeleybross@gmail.com>
2019-04-20 15:08:47 +0100

    Added clone of nuxt,js fork

--- cloneAllRepos.ps1
+++ cloneAllRepos.ps1
@@ -4,3 +4,8 @@ If(!(test-path ~/git/PeregrineDb))
 {
     git clone https://github.com/berkeleybross/PeregrineDb ~/git/PeregrineDb
 }
+
+If(!(test-path ~/git/nuxt.js))
+{
+    git clone https://github.com/berkeleybross/nuxt.js ~/git/nuxt.js
+}
