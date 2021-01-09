981ef2013dbd8da868b8a9e520b1d31997a0ae8e
berkeleybross <berkeleybross@gmail.com>
2021-01-09 17:20:46 +0000

    Fixed run all script

---
+++ run-all.ps1
@@ -0,0 +1,22 @@
+#!/usr/bin/env pwsh
+#requires -Version 7
+
+# This script is a convienience wrapper that will run all appropriate scripts in the correct order.
+# If this is the first time using this repository, please read the README and follow the appropriate steps for your OS.
+param()
+
+$ErrorActionPreference = "Stop"
+. $PsScriptRoot/scripts/helpers.ps1
+
+if (Test-Administrator) {
+    Throw "This script must NOT be run as administrator/root"
+}
+
+Push-Location "$PsScriptRoot/scripts"
+try {
+    sudo ./install-apps_ubuntu.sh
+    ./install-vscode-extensions.ps1
+}
+finally {
+    Pop-Location
+}
--- scripts/helpers.ps1
+++ scripts/helpers.ps1
@@ -1,6 +1,30 @@
+Function Exec() {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)][scriptblock]$cmd,
+        [String]$message = "Failed to run $cmd"
+    )
+
+    & $cmd
+
+    if ($global:LastExitCode -ne 0) {
+        throw $message
+    }
+}
+
 function Test-Administrator {
     if ($IsLinux) {
         return ((id -u) -eq 0)
+    }
+
+    throw "Unknown operating system"
+}
+
+function Sudo {
+    if ($IsLinux) {
+        $parameters = $args
+        Exec { bash -c "sudo $parameters"} -Message "Failed to sudo $parameters"
+        return
     }
 
     throw "Unknown operating system"
